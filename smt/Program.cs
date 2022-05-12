f1258624f1c5dbe3bfa7c8a638222ed0df039173
Katherine_20 <mob1911494@gmail.com>
2022-05-12 20:17:06 +0300

    added smt

---
+++ smt/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write number: ");
+
+string numberStr = Console.ReadLine();
+
+int firstNumber = int.Parse(numberStr);
+int secondNumber = Convert.ToInt32(numberStr);
+int thirdNumber = 8;
+
+if (int.TryParse(numberStr, out int numberValue))
+thirdNumber = numberValue;
+bool needToCastToInt = true;
