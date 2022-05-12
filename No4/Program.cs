4653c8ec7fd70b168d4b23bc97a36e7ba79ad657
Katherine_20 <mob1911494@gmail.com>
2022-05-12 21:05:03 +0300

    No4 which proves if a number can be divided by 7 & 23

--- No4/Program.cs
+++ No4/Program.cs
@@ -1,12 +1,9 @@
 ﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num % 7 == 0)
+if ((num % 7 == 0) && (num % 23 == 0))
 {
-    if (num % 23 == 0) 
-    {
        Console.WriteLine($"Число {num} кратно 7 и 23");
-    }
 }
 else 
 {       
