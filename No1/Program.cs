635a6f12744b76db4c8052f6339b0e25c88607cc
Katherine_20 <mob1911494@gmail.com>
2022-05-12 20:24:08 +0300

    No1Random 2 numbers and max finding

---
+++ No1/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine($"Сгенерированное число: {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+Console.WriteLine($"First number: {firstDigit}, second number: {secondDigit}");
+
+int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+Console.WriteLine($"max number is: {max}");
